029acbec8fc8a2b4fbb354332112c69fc400ea37
noaihere <44403965+noaihere@users.noreply.github.com>
2020-04-19 02:25:19 -0700

    app mine

--- app.py
+++ app.py
@@ -1,63 +1,30 @@
+import pandas as pd
+import plotly.express as px
+df=pd.read_csv('https://raw.githubusercontent.com/noaihere/cov_dailychart/master/total_df.csv')
+df_com=pd.read_csv('https://raw.githubusercontent.com/noaihere/cov_dailychart/master/com_df.csv')
+t=df.merge(df_com, on='metric', how='left')
+t['value']=t.apply(lambda x: x["value_y"] if pd.notnull(x["value_y"]) else x["value_x"], axis=1)
+
+t = t.loc[t.category_1=='additional',:]
+t= t[['metric', 'category_2','value']]
+fig = px.sunburst(t, path=['metric', 'category_2'], values='value',color='metric', color_discrete_map={'community':'gold'})
+fig.update_layout(uniformtext_minsize=52, 
+    margin=dict(l=0, r=0, t=0, b=0),
+    paper_bgcolor="LightSteelBlue",
+)
+
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
+app = dash.Dash()
 app.layout = html.Div(children=[
-    html.H1(myheading),
+    html.H1("daily charts"),
     dcc.Graph(
         id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
+        figure=fig
+    )])
 
 if __name__ == '__main__':
     app.run_server()
